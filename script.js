65f66e6fb19875cf7834e2e1885f3ca0723a1505
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-06 12:57:00 +0200

    Create operate function for calling the correct calculator function based on the provided  operator

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2
 }
+
+let num1
+let num2
+let operator
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2)
+        default:
+            return "No support for that operator. Try one of these + - * /"
+        }
+}
